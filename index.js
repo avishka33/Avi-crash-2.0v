80445a2680fa67c9bfdd409f48025979f6f26077
AVISHKA SHAVINDA <134716496+avishka33@users.noreply.github.com>
2024-05-01 15:03:32 +0530

    2.0 v

---
+++ index.js
@@ -0,0 +1,32 @@
+//base by Avishka shavinda
+//YouTube: @AviNetHunter
+//Telegram: t.me/AviNetHunter
+//GitHub: @avishka33
+//WhatsApp: +94772836332
+//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@Avi_NetHunter?sub_confirmation=1
+
+const {
+   spawn
+} = require('child_process')
+const path = require('path')
+
+function start() {
+   let args = [path.join(__dirname, 'main.js'), ...process.argv.slice(2)]
+   console.log([process.argv[0], ...args].join('\n'))
+   let p = spawn(process.argv[0], args, {
+         stdio: ['inherit', 'inherit', 'inherit', 'ipc']
+      })
+      .on('message', data => {
+         if (data == 'reset') {
+            console.log('Restarting AVI ...')
+            p.kill()
+            start()
+            delete p
+         }
+      })
+      .on('exit', code => {
+         console.error('Exited with code:', code)
+         if (code == '.' || code == 1 || code == 0) start()
+      })
+}
+start()
